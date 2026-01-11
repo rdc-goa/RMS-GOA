6130446b8ce980e5b4125b14b8282dc07144a280
Pranav rathi <rathipranav07@gmail.com>
2026-01-11 08:31:37 +0000

    Try fixing this error: `Console Error: [GSI_LOGGER]: FedCM get() rejects

--- next.config.ts
+++ next.config.ts
@@ -12,12 +12,16 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
-        source: '/(.*)',
+        source: '/:path*',
         headers: [
           {
             key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin-allow-popups',
+            value: 'same-origin',
           },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+          }
         ],
       },
     ];
